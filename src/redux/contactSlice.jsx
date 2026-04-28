fa1184fe22759c009806ec5d6dbb4c2862239dbc
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2026-04-27 22:54:41 -0500

    cambiar url para obtener imagenes en los contactos

--- src/redux/contactSlice.jsx
+++ src/redux/contactSlice.jsx
@@ -19,7 +19,7 @@ const initialState = {
         "isDeleted" :false,
         "first_name": "George",
         "last_name": "Bluth",
-        "avatar": "https://reqres.in/img/faces/1-image.jpg",
+        "avatar": "https://i.pravatar.cc/150?img=1",
       },
       {
         "id": 2,
@@ -28,7 +28,7 @@ const initialState = {
         "isDeleted" :false,
         "first_name": "Janet",
         "last_name": "Weaver",
-        "avatar": "https://reqres.in/img/faces/2-image.jpg"
+        "avatar": "https://i.pravatar.cc/150?img=2"
       },
       {
         "id": 3,
@@ -37,7 +37,7 @@ const initialState = {
         "isDeleted" :false,
         "first_name": "Emma",
         "last_name": "Wong",
-        "avatar": "https://reqres.in/img/faces/3-image.jpg"
+        "avatar": "https://i.pravatar.cc/150?img=3"
       },
       {
         "id": 4,
@@ -46,7 +46,7 @@ const initialState = {
         "isDeleted" :false,
         "first_name": "Eve",
         "last_name": "Holt",
-        "avatar": "https://reqres.in/img/faces/4-image.jpg"
+        "avatar": "https://i.pravatar.cc/150?img=4"
       },
       {
         "id": 5,
@@ -55,7 +55,7 @@ const initialState = {
         "isDeleted" :false,
         "first_name": "Charles",
         "last_name": "Morris",
-        "avatar": "https://reqres.in/img/faces/5-image.jpg"
+        "avatar": "https://i.pravatar.cc/150?img=5"
       },
       {
         "id": 6,
@@ -64,7 +64,7 @@ const initialState = {
         "isDeleted" :false,
         "first_name": "Tracey",
         "last_name": "Ramos",
-        "avatar": "https://reqres.in/img/faces/6-image.jpg"
+        "avatar": "https://i.pravatar.cc/150?img=6"
       },
       {
         "id": 7,
@@ -73,7 +73,7 @@ const initialState = {
         "isDeleted" :false,
         "first_name": "Michael",
         "last_name": "Lawson",
-        "avatar": "https://reqres.in/img/faces/7-image.jpg"
+        "avatar": "https://i.pravatar.cc/150?img=7"
       },
       {
         "id": 8,
@@ -82,7 +82,7 @@ const initialState = {
         "isDeleted" :false,
         "first_name": "Lindsay",
         "last_name": "Ferguson",
-        "avatar": "https://reqres.in/img/faces/8-image.jpg"
+        "avatar": "https://i.pravatar.cc/150?img=8"
       },
       {
         "id": 9,
@@ -91,7 +91,7 @@ const initialState = {
         "isDeleted" :false,
         "first_name": "Tobias",
         "last_name": "Funke",
-        "avatar": "https://reqres.in/img/faces/9-image.jpg"
+        "avatar": "https://i.pravatar.cc/150?img=9"
       },
       {
         "id": 10,
@@ -100,7 +100,7 @@ const initialState = {
         "isDeleted" :false,
         "first_name": "Byron",
         "last_name": "Fields",
-        "avatar": "https://reqres.in/img/faces/10-image.jpg"
+        "avatar": "https://i.pravatar.cc/150?img=10"
       },
       {
         "id": 11,
@@ -109,7 +109,7 @@ const initialState = {
         "isDeleted" :false,
         "first_name": "George",
         "last_name": "Edwards",
-        "avatar": "https://reqres.in/img/faces/11-image.jpg"
+        "avatar": "https://i.pravatar.cc/150?img=11"
       },
       {
         "id": 12,
@@ -118,7 +118,7 @@ const initialState = {
         "isDeleted" :false,
         "first_name": "Rachel",
         "last_name": "Howell",
-        "avatar": "https://reqres.in/img/faces/12-image.jpg"
+        "avatar": "https://i.pravatar.cc/150?img=12"
       }
     ], 
 }
